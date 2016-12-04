a7c1acf03e21f19d42e7b684db5d54a491acd5fa
Rishi Khanna <rishik@andrew.cmu.edu>
2016-12-04 15:50:53 -0500

    generateData

--- reconstruction/parseFiles.m
+++ reconstruction/parseFiles.m
@@ -1,14 +1,19 @@
-function [names,data] = parseFiles(files)
+function [names,data] = parseFiles(data_dir)
 %parses directory struct into file names and file data
 %input: directory struct
 %output: names - cell of names
 %        data - cell of file data
+
+files = dir([data_dir '*.wav']);
 names = cell(1,size(files,1));
 data = cell(1,size(files,1));
 for i=1:size(files,1)
-    file = files(i,:);
+    file = files(i);
     names{i} = file.name;
-    data{i} = audioread(names{i});
+    [m, fs] = audioread([data_dir names{i}]);
+    data{i} = cell(2,1);
+    data{i}{1} = mean(m,2);
+    data{i}{2} = fs; 
 end
 
 end

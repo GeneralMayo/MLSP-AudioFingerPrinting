cdcf4248a1efffade7c6ee196cd500d66b4a066d
Rishi Khanna <rishik@andrew.cmu.edu>
2016-12-04 21:14:51 -0500

    Fixed get_peaks 0 edge case

--- reconstruction/generateAddresses.m
+++ reconstruction/generateAddresses.m
@@ -1,4 +1,4 @@
-function [ addresses ] = generateAddresses(audio)
+function [ addresses ] = generateAddresses(audio, id)
 % given an audio file, generates the set of addresses for that file
 %input: 2x1 Audio cell, where Row 1 is the data, Row 2 is sfs
 %output: Nx1 matrix of addresses, where each addresses is a 32 bit int
@@ -14,6 +14,6 @@ fs = audio{2};
 peaks = get_peaks(S);
 
 %hash peaks to addresses
-addresses = hashing(peaks,TARGET_ZONE_SIZE,ANCHOR_POSITION);
+addresses = hashing(peaks,TARGET_ZONE_SIZE,ANCHOR_POSITION, id);
 end
 

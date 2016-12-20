f83000f70c13acc73bc2affda224f9266391c41b
GeneralMayo <tmayosmith@gmail.com>
2016-12-19 22:35:37 -0500

    Delete Test Files
    Deleted all extra files outside of reconstruction folder

--- structTest.m
+++
@@ -1,7 +0,0 @@
-xxx = struct('elem1',struct('elem11',1,'elem12',2));
-xxx.('elem2') = struct('elem21',1,'elem22',2,'elem23',3);
-elem1Xs = xxx.elem1;
-elem1Xs.elem11 = elem1Xs.elem11+1;
-elem1Xs.elem11
-xxx.elem1.elem11
-
